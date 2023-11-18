621b13bf409210bac2dd44941c2c17337e860586
jonahbaer <25778692+jonahbaer@users.noreply.github.com>
2023-11-18 14:21:12 -0500

    client setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const conf = require('./config');
-const routes = require('./routes');
+import express from 'express';
+import config from './config';
+import routes from './routes';
 
 const app = express();
 
 app.get('/test', routes.test);
 
-app.listen(conf.server_port, () => {
-    console.log(`Server running at http://${conf.server_host}:${conf.server_port}/`)
+app.listen(config.server_port, () => {
+    console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
 });
