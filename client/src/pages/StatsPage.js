e3d49d6b22688c423a772a23e091ded41675f1fb
kyle1iao <kyleliao@sas.upenn.edu>
2023-12-15 17:28:03 -0500

    Add visualizations

--- client/src/pages/StatsPage.js
+++ client/src/pages/StatsPage.js
@@ -1,6 +1,6 @@
-import {Container, Grid, Typography} from "@mui/material";
-import {useEffect, useState} from "react";
-import {Bar, BarChart, Legend, Tooltip, XAxis, YAxis} from "recharts";
+import { Container, Grid, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis, LineChart, CartesianGrid, Line, Label } from "recharts";
 import config from "../config.json";
 
 const CustomizedYAxisTick = (props) => {
@@ -33,26 +33,94 @@ export default function StatsPage() {
             .then(resJson => setMostAlbumsData(resJson));
     }, []);
 
+    const [musicTrendsData, setMusicTrendsData] = useState([]);
+    useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/music_trends?page_size=10&sorted=true`)
+            .then(res => res.json())
+            .then(resJson => setMusicTrendsData(resJson));
+    }, []);
+
+    const [annualSongsData, setAnnualSongsData] = useState([]);
+    useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/songs_per_year?page_size=10&sorted=true`)
+            .then(res => res.json())
+            .then(resJson => setAnnualSongsData(resJson));
+    }, []);
+
+    const [explicitSongsData, setExplicitSongsData] = useState([]);
+    useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/explicit_songs_per_year?page_size=10&sorted=true`)
+            .then(res => res.json())
+            .then(resJson => setExplicitSongsData(resJson));
+    }, []);
+
     return (
-        <Grid container spacing={2} align='center' style={{ paddingTop:'100px' }}>
+
+        <Grid container spacing={10} align='center' style={{ paddingTop: '100px' }}>
             <Grid item xs={6}>
-                <Typography variant='h5' align='center'>most songs</Typography>
-                <BarChart width={600} height={300} data={topArtistData} margin={{bottom: 40}}>
-                    <XAxis dataKey="artist_name" type="category" interval={0} tick={<CustomizedYAxisTick />}/>
-                    <YAxis/>
-                    <Bar dataKey="total_songs"/>
-                    <Tooltip/>
+                <Typography variant='h5' align='center'>Artists with Most Songs</Typography>
+                <BarChart width={600} height={300} data={topArtistData} margin={{ bottom: 40 }}>
+                    <XAxis dataKey="artist_name" type="category" interval={0} tick={<CustomizedYAxisTick />} />
+                    <YAxis />
+                    <Bar dataKey="total_songs" />
+                    <Tooltip />
                 </BarChart>
             </Grid>
+
             <Grid item xs={6}>
-                <Typography variant='h5' align='center'>most albums</Typography>
-                <BarChart width={600} height={300} data={mostAlbumsData} margin={{bottom: 40}}>
-                    <XAxis dataKey="artist_name" type="category" interval={0} tick={<CustomizedYAxisTick />}/>
-                    <YAxis/>
-                    <Bar dataKey="total_albums"/>
-                    <Tooltip/>
+                <Typography variant='h5' align='center'>Artists with Most Albums</Typography>
+                <BarChart width={600} height={300} data={mostAlbumsData} margin={{ bottom: 40 }}>
+                    <XAxis dataKey="artist_name" type="category" interval={0} tick={<CustomizedYAxisTick />} />
+                    <YAxis />
+                    <Bar dataKey="total_albums" />
+                    <Tooltip />
                 </BarChart>
             </Grid>
+
+            <Grid item xs={6}>
+                <Typography variant='h5' align='center'>Yearly Trends Across Songs</Typography>
+                <LineChart width={730} height={250} data={musicTrendsData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="avg_danceability" stroke="#f47068" dot={false} />
+                    <Line type="monotone" dataKey="avg_energy" stroke="#ffb3ae" dot={false} />
+                    <Line type="monotone" dataKey="avg_acousticness" stroke="#f78c6b" dot={false} />
+                    <Line type="monotone" dataKey="avg_instrumentalness" stroke="#1697a6" dot={false} />
+                    <Line type="monotone" dataKey="avg_liveness" stroke="#0e606b" dot={false} />
+                    <Line type="monotone" dataKey="avg_valence" stroke="#ffc24b" dot={false} />
+                </LineChart>
+            </Grid>
+
+            <Grid item xs={6}>
+                <Typography variant='h5' align='center'>Number of Songs Per Year</Typography>
+                <LineChart width={730} height={250} data={annualSongsData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line dataKey="num_songs" stroke="#f47068" dot={false} />
+                </LineChart>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Typography variant='h5' align='center'>Number of Explicit Songs Per Year</Typography>
+                <LineChart width={730} height={250} data={explicitSongsData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line dataKey="num_explicit_songs" stroke="#f47068" dot={false} />
+                </LineChart>
+            </Grid>
+
         </Grid>
     )
 }
